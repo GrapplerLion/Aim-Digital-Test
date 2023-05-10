a33b3ec3ea8945ff17bf15b5164556956ee0af20
Dahak123 <103024805+Dahak123@users.noreply.github.com>
2023-05-10 05:24:22 +0500

    added step

--- src/test/java/demotest/pages/components/WebSteps.java
+++ src/test/java/demotest/pages/components/WebSteps.java
@@ -3,6 +3,7 @@ package demotest.pages.components;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.Disabled;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 @Disabled
@@ -10,6 +11,13 @@ public class WebSteps {
     @Step("Открываем главную страницу")
     public void openMainPage() {
         open("https://www.in-aim.ru/");
+    }
+
+    @Step("Ищем репозиторий {repository}")
+    public void searchForRepository(String repository) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repository);
+        $(".header-search-input").submit();
     }
 
 }
